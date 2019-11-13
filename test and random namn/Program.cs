9b2b280b603083c21e19bd54c9056ce91b144632
jacob jolsö <jacke_j_1998@hotmail.se>
2019-11-13 10:35:58 +0100

    Add project files.

---
+++ test and random namn/Program.cs
@@ -0,0 +1,20 @@
+﻿using Lexicon.CSharp.InfoGenerator;
+using System;
+
+namespace test_and_random_namn
+{
+    class Program
+    {
+        static InfoGenerator infoGen = new InfoGenerator(DateTime.Now.Millisecond);
+
+        static void Main(string[] args)
+        {
+            for (int i =0; i <5; i++)
+            {
+
+            Console.WriteLine(infoGen.NextAnimal());
+            }
+
+        }
+    }
+}
